bbadfdfdf6f871c99e237cf862a14744b5730ce0
Jayaprakash Solaisomasundharam <jayaprakash.solaisomasundharam@alineops.com>
2025-07-14 20:39:32 +0530

    test

--- Module1/main.tf
+++ Module1/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
     
   }
 }
-resource "azurerm_resource_group" "myrg" {
+resource "azurerm_resource_group" "my-rg" {
   name = var.resource_group_name
   location = var.location
 }

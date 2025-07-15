e58afdc26eba8b68ac55562dae09e103ec267c33
Jash080903 <jey03prakash@gmail.com>
2025-07-15 14:57:49 +0530

    Update provider.tf

--- Module1/provider.tf
+++ Module1/provider.tf
@@ -8,8 +8,7 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  client_id       = ""
-  client_secret   = ""
-  tenant_id       = ""
-  subscription_id = ""
+  client_id       = "a1b6efc6-0166-4f2d-ad6c-ff47993e6592"
+  tenant_id       = "9ac6a4ee-3fd5-4e9a-a998-13b5762fdec6"
+  subscription_id = "7ba96dd6-bf3c-49c4-81d8-1bd1c5a97c90"
 }  
